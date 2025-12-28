afd42ee066271f6bb7e2ca52188bb5948af3b176
Katelyn Grimoldby <katelyngrimoldby@gmail.com>
2025-12-28 14:37:57 -0700

    chore: Add postcss plugins

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,17 @@
 const config = {
   plugins: {
-    autoprefixer: {},
+    'postcss-flexbugs-fixes': {},
+    'postcss-preset-env': {
+      autoprefixer: {
+        flexbox: 'no-2009',
+      },
+      stage: 3,
+      features: {
+        'custom-properties': false,
+      },
+    },
+    'postcss-relative-opacity': {},
+    'postcss-nesting': {},
   },
 };
 
