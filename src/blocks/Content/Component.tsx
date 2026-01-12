629e2eebd55afdbee775fe5439885806aa64d19e
Katelyn Grimoldby <katelyngrimoldby@gmail.com>
2026-01-11 18:33:03 -0700

    feat: style content block

--- src/blocks/Content/Component.tsx
+++ src/blocks/Content/Component.tsx
@@ -3,20 +3,27 @@ import RichText from '@/components/RichText';
 
 import type { ContentBlock as ContentBlockProps } from '@/payload-types';
 
+import styles from './style.module.css';
+
 export const ContentBlock: React.FC<ContentBlockProps> = (props) => {
   const { width, theme, heading, columns } = props;
 
   return (
-    <div className={`${width}. ${theme}`}>
-      {heading && <RichText data={heading} enableGutter={false} type="heading" />}
-      <div>
+    <div className={`${styles.container} ${theme}`}>
+      <div className={`${styles.wrapper} ${width}`}>
+        {heading && (
+          <div className={styles.heading}>
+            <RichText data={heading} type="heading" />
+          </div>
+        )}
         {columns &&
           columns.length > 0 &&
           columns.map((col, index) => {
-            const { content } = col;
-
+            const { content, size } = col;
             return (
-              <div key={index}>{content && <RichText data={content} enableGutter={false} />}</div>
+              <div key={index} className={`${styles.column} ${size}`}>
+                {content && <RichText data={content} />}
+              </div>
             );
           })}
       </div>
